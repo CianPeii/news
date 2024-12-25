c29399a09358c0b6d0b43138c3d3b1639ea02a7a
Pei <n0988040989@gmail.com>
2024-12-26 04:30:22 +0800

    revise   categories object  &  revise  title css

--- src/pages/NewsCategory.jsx
+++ src/pages/NewsCategory.jsx
@@ -14,28 +14,40 @@ import {
 function NewsCategory() {
   let { category } = useParams();
   const categories = [
-    { name: "GENERAL", Icon: Globe },
-    { name: "BUSINESS", Icon: Briefcase },
-    { name: "TECHNOLOGY", Icon: CircuitBoard },
-    { name: "ENTERTAINMENT", Icon: Film },
-    { name: "SPORTS", Icon: Trophy },
-    { name: "SCIENCE", Icon: Microscope },
-    { name: "HEALTH", Icon: Heart },
+    { name: "general", Icon: Globe },
+    { name: "business", Icon: Briefcase },
+    { name: "technology", Icon: CircuitBoard },
+    { name: "entertainment", Icon: Film },
+    { name: "sports", Icon: Trophy },
+    { name: "science", Icon: Microscope },
+    { name: "health", Icon: Heart },
   ];
-  const upperCategory = category.toUpperCase();
-  const CategoryIcon = categories.find(
-    (item) => item.name === upperCategory
-  )?.Icon;
+
+  const CategoryIcon = categories.find((item) => item.name === category)?.Icon;
 
   return (
     <>
       <Header />
       <Navbar />
-      <div className="flex items-center space-x-3">
-        {CategoryIcon && <CategoryIcon className="w-6 h-6" />}
-        <h1 className="text-3xl">
-          {category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
-        </h1>
+      <div className="py-6 px-8 ">
+        <div className="flex items-center gap-4 border-l-4 border-blue-500 pl-4">
+          <div className="flex items-center gap-3">
+            {CategoryIcon && (
+              <div className="p-3 bg-blue-50 rounded-lg">
+                <CategoryIcon className="w-6 h-6 text-blue-600" />
+              </div>
+            )}
+
+            <div>
+              <h1 className="text-3xl font-bold text-gray-800">
+                {category.charAt(0).toUpperCase() +
+                  category.slice(1).toLowerCase()}
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className=" py-6 px-8">new1111</div>
       </div>
     </>
   );
