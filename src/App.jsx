43a965030e183868066e041be0c707515a29b57c
Pei <n0988040989@gmail.com>
2024-12-27 23:54:50 +0800

    add Secarch element  Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Home from "./pages/Home";
 import NewsCategory from "./pages/NewsCategory";
+import ArticleDetail from "./pages/ArticleDetail";
+import Search from "./pages/Search";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+
 function App() {
   const root = document.getElementById("root");
   ReactDOM.createRoot(root).render(
@@ -9,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:category" element={<NewsCategory />} />
+        <Route path="/article/:title" element={<ArticleDetail />} />
+        <Route path="/search/:keyword" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
