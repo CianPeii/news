37c24c7a2e05dc30010217cf625d00108210e793
Pei <n0988040989@gmail.com>
2024-12-31 02:19:49 +0800

    change Loading css

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-red-600" />
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-sky-400" />
       <p className="mt-4 text-lg text-gray-600 animate-pulse">Loading...</p>
     </div>
   );
