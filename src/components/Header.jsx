91672641add0e28f33384af51f2c7aa930d40c70
Pei <n0988040989@gmail.com>
2025-01-17 03:17:22 +0800

    fix sidebar css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,159 +66,161 @@ function Header() {
           className="text-gray-400 hover:text-sky-500 transition-colors duration-200 cursor-pointer"
         />
       </header>
-      {isSidebarOpen ? (
-        <div
-          className="fixed inset-0 bg-black/20 backdrop-blur-sm z-50"
-          onClick={(e) => {
-            if (e.target === e.currentTarget) {
-              setIsSidebarOpen(false);
-            }
-          }}
-        >
-          <div className="absolute inset-y-0 right-0 w-80 flex">
-            {/* 側邊欄主體 */}
-            <div className="w-full bg-white dark:bg-slate-800 shadow-2xl">
-              {/*Menu */}
-              <div className="flex justify-between items-center px-6 py-4 border-b border-gray-100">
-                <h1 className="text-2xl font-semibold text-slate-900 dark:text-white">
-                  {t("menu")}
-                </h1>
-                <button
-                  className="p-2 rounded-full 
+      <div
+        className={`fixed inset-0 bg-black/20 backdrop-blur-sm z-50 transition-all duration-300 ease-in-out
+  ${isSidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+        onClick={(e) => {
+          if (e.target === e.currentTarget) {
+            setIsSidebarOpen(false);
+          }
+        }}
+      >
+        <div className="absolute inset-y-0 right-0 w-80 flex">
+          {/* 側邊欄主體 */}
+          <div
+            className={`w-full bg-white dark:bg-slate-800 shadow-2xl transform transition-transform duration-300 ease-in-out
+              ${isSidebarOpen ? "translate-x-0" : "translate-x-full"}`}
+          >
+            {" "}
+            {/*Menu */}
+            <div className="flex justify-between items-center px-6 py-4 border-b border-gray-100">
+              <h1 className="text-2xl font-semibold text-slate-900 dark:text-white">
+                {t("menu")}
+              </h1>
+              <button
+                className="p-2 rounded-full 
                      bg-gray-50   dark:bg-slate-900  hover:bg-sky-500 group
                      transform transition-all duration-300 ease-in-out
                      hover:rotate-90 hover:scale-110
                      active:scale-95
                      shadow-sm hover:shadow-md"
-                  onClick={() => setIsSidebarOpen(false)}
-                >
-                  <X
-                    className="w-5 h-5 text-gray-600 group-hover:text-white
+                onClick={() => setIsSidebarOpen(false)}
+              >
+                <X
+                  className="w-5 h-5 text-gray-600 group-hover:text-white
                        transition-colors duration-300"
-                  />
-                </button>
-              </div>
-
-              {/* 選項列表區域 */}
-              <div className="flex flex-col">
-                {/* 語言選擇 */}
-                <button
-                  className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
+                />
+              </button>
+            </div>
+            {/* 選項列表區域 */}
+            <div className="flex flex-col">
+              {/* 語言選擇 */}
+              <button
+                className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
                            transition-colors duration-200 border-b border-gray-100 group"
-                  onClick={() => {
-                    toggleLanguageMenu();
-                  }}
+                onClick={() => {
+                  toggleLanguageMenu();
+                }}
+              >
+                <Globe
+                  className="w-5 h-5 text-gray-500 group-hover:text-sky-500 
+                          transition-colors duration-200"
+                />
+                <span
+                  className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
+                          transition-colors duration-200"
                 >
-                  <Globe
-                    className="w-5 h-5 text-gray-500 group-hover:text-sky-500 
-                          transition-colors duration-200"
-                  />
-                  <span
-                    className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
-                          transition-colors duration-200"
-                  >
-                    {t("language")}
-                    {isLanguageMenuOpen ? (
-                      <ChevronUp className="inline" size={18} />
-                    ) : (
-                      <ChevronDown className="inline" size={18} />
-                    )}
-                  </span>
-                </button>
-                {/* 語言選單 */}
-                {isLanguageMenuOpen ? (
-                  <>
-                    <button
-                      className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
-                  transition-colors duration-200 border-b border-gray-100 group"
-                      onClick={() => changeLanguage("en")}
-                    >
-                      <div className="w-5">
-                        <img className="w-full" src={us} alt="us" />
-                      </div>
-                      <span
-                        className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
-                          transition-colors duration-200"
-                      >
-                        English
-                      </span>
-                    </button>
-                    <button
-                      className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
-                     transition-colors duration-200 border-b border-gray-100 group"
-                      onClick={() => changeLanguage("zh")}
-                    >
-                      <div className="w-5">
-                        <img className="w-full" src={tw} alt="tw" />
-                      </div>
-                      <span
-                        className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
-                          transition-colors duration-200"
-                      >
-                        繁體中文
-                      </span>
-                    </button>
-                  </>
-                ) : null}
-
-                {/* 儲存新聞 */}
-                <button
-                  className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
-                           transition-colors duration-200 border-b border-gray-100 group"
-                  onClick={() => {
-                    navigate("/bookmarks");
-                  }}
-                >
-                  <Bookmark
-                    className="w-5 h-5 text-gray-500 group-hover:text-sky-500
-                             transition-colors duration-200"
-                  />
-                  <span
-                    className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
-                          transition-colors duration-200"
-                  >
-                    {t("savedNews")}
-                  </span>
-                </button>
-                {/* 黑暗模式設置 */}
-                <div
-                  className="flex items-center space-x-3 px-6 py-4 border-b border-gray-100
-                        hover:bg-sky-50 transition-colors duration-200 group"
-                >
-                  <Palette
-                    className="w-5 h-5 text-gray-500 group-hover:text-sky-500 
-                          transition-colors duration-200"
-                  />
-                  <span className="text-lg text-gray-700 dark:text-white  group-hover:text-gray-900">
-                    {t("darkMode")}
-                  </span>
+                  {t("language")}
+                  {isLanguageMenuOpen ? (
+                    <ChevronUp className="inline" size={18} />
+                  ) : (
+                    <ChevronDown className="inline" size={18} />
+                  )}
+                </span>
+              </button>
+              {/* 語言選單 */}
+              {isLanguageMenuOpen && (
+                <>
                   <button
-                    onClick={handleThemeToggle}
-                    className={`relative inline-flex h-7 w-12 items-center rounded-full 
+                    className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
+                  transition-colors duration-200 border-b border-gray-100 group"
+                    onClick={() => changeLanguage("en")}
+                  >
+                    <div className="w-5">
+                      <img className="w-full" src={us} alt="us" />
+                    </div>
+                    <span
+                      className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
+                          transition-colors duration-200"
+                    >
+                      English
+                    </span>
+                  </button>
+                  <button
+                    className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
+                     transition-colors duration-200 border-b border-gray-100 group"
+                    onClick={() => changeLanguage("zh")}
+                  >
+                    <div className="w-5">
+                      <img className="w-full" src={tw} alt="tw" />
+                    </div>
+                    <span
+                      className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
+                          transition-colors duration-200"
+                    >
+                      繁體中文
+                    </span>
+                  </button>
+                </>
+              )}
+
+              {/* 儲存新聞 */}
+              <button
+                className="flex items-center space-x-3 px-6 py-4 hover:bg-sky-50 
+                           transition-colors duration-200 border-b border-gray-100 group"
+                onClick={() => {
+                  navigate("/bookmarks");
+                }}
+              >
+                <Bookmark
+                  className="w-5 h-5 text-gray-500 group-hover:text-sky-500
+                             transition-colors duration-200"
+                />
+                <span
+                  className="text-lg text-gray-700 dark:text-white group-hover:text-gray-900
+                          transition-colors duration-200"
+                >
+                  {t("savedNews")}
+                </span>
+              </button>
+              {/* 黑暗模式設置 */}
+              <div
+                className="flex items-center space-x-3 px-6 py-4 border-b border-gray-100
+                        hover:bg-sky-50 transition-colors duration-200 group"
+              >
+                <Palette
+                  className="w-5 h-5 text-gray-500 group-hover:text-sky-500 
+                          transition-colors duration-200"
+                />
+                <span className="text-lg text-gray-700 dark:text-white  group-hover:text-gray-900">
+                  {t("darkMode")}
+                </span>
+                <button
+                  onClick={handleThemeToggle}
+                  className={`relative inline-flex h-7 w-12 items-center rounded-full 
                        transition-colors duration-300 ease-in-out
                        focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2
                        ${isDark ? "bg-gray-700 " : "bg-gray-200 "}`}
-                  >
-                    <span
-                      className={`inline-block h-5 w-5 transform rounded-full bg-white 
+                >
+                  <span
+                    className={`inline-block h-5 w-5 transform rounded-full bg-white 
                          shadow-md transition-transform duration-300 ease-in-out
                          ${isDark ? "translate-x-6" : "translate-x-1"}`}
-                    >
-                      <span className="flex h-full w-full items-center justify-center">
-                        {isDark ? (
-                          <Moon className="h-3 w-3 text-gray-600" />
-                        ) : (
-                          <Sun className="h-3 w-3 text-gray-600" />
-                        )}
-                      </span>
+                  >
+                    <span className="flex h-full w-full items-center justify-center">
+                      {isDark ? (
+                        <Moon className="h-3 w-3 text-gray-600" />
+                      ) : (
+                        <Sun className="h-3 w-3 text-gray-600" />
+                      )}
                     </span>
-                  </button>
-                </div>
+                  </span>
+                </button>
               </div>
             </div>
           </div>
         </div>
-      ) : null}
+      </div>
     </>
   );
 }
